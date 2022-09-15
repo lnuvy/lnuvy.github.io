05ef311f8abfa697d0a1cc57824607fb0a5424c5
lnuvy <lnuvy.code@gmail.com>
2022-09-16 03:29:45 +0900

    commit

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   swcMinify: true,
-// }
-//
-// module.exports = nextConfig
-
-const debug = process.env.NODE_ENV !== 'production'
-const name = 'lnuvy.github.io'
-
-module.exports = {
-  assetPrefix: !debug ? `/${name}/` : '',
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    loader: 'akamai',
+    path:  "",
+  }
 }
+
+module.exports = nextConfig
