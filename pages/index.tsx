19454789210dfed2d6652196b9691cd9b58bd35e
lnuvy <lnuvy.code@gmail.com>
2022-09-17 23:21:43 +0900

    grid 참고

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import styled from '@emotion/styled'
-import styles from '../styles/Home.modules.scss'
+import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
